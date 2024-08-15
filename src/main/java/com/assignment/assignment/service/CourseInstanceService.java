b035cd29c5934f728d51b32a9b82b25f3fd92309
Immortal <ak2061435@gmail.com>
2024-08-15 11:19:53 +0530

    add service

---
+++ src/main/java/com/assignment/assignment/service/CourseInstanceService.java
@@ -0,0 +1,32 @@
+package com.assignment.assignment.service;
+
+import com.assignment.assignment.model.CourseInstance;
+import com.assignment.assignment.repository.CourseInstanceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseInstanceService {
+
+    @Autowired
+    private CourseInstanceRepository courseInstanceRepository;
+
+    public CourseInstance createCourseInstance(CourseInstance courseInstance) {
+        return courseInstanceRepository.save(courseInstance);
+    }
+
+    public List<CourseInstance> getInstancesByYearAndSemester(int year, int semester) {
+        return courseInstanceRepository.findByYearAndSemester(year, semester);
+    }
+
+    public Optional<CourseInstance> getCourseInstance(Long id) {
+        return courseInstanceRepository.findById(id);
+    }
+
+    public void deleteCourseInstance(Long id) {
+        courseInstanceRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/assignment/assignment/service/CourseService.java
@@ -0,0 +1,32 @@
+package com.assignment.assignment.service;
+
+import com.assignment.assignment.model.Course;
+import com.assignment.assignment.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public Course createCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public Optional<Course> getCourseById(Long id) {
+        return courseRepository.findById(id);
+    }
+
+    public void deleteCourse(Long id) {
+        courseRepository.deleteById(id);
+    }
+}
