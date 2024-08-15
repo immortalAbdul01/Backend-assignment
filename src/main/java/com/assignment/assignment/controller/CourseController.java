379252841791391f7c2ad1bc4e24a48a93451757
Immortal <ak2061435@gmail.com>
2024-08-15 11:15:49 +0530

    add controller

---
+++ src/main/java/com/assignment/assignment/controller/CourseController.java
@@ -0,0 +1,36 @@
+package com.assignment.assignment.controller;
+
+import com.assignment.assignment.model.Course;
+import com.assignment.assignment.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @PostMapping
+    public Course createCourse(@RequestBody Course course) {
+        return courseService.createCourse(course);
+    }
+
+    @GetMapping
+    public List<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @GetMapping("/{id}")
+    public Course getCourseById(@PathVariable Long id) {
+        return courseService.getCourseById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCourse(@PathVariable Long id) {
+        courseService.deleteCourse(id);
+    }
+}
--- src/main/java/com/assignment/assignment/controllers/CourseController.java
+++
@@ -1,4 +0,0 @@
-package com.assignment.assignment.controllers;
-
-public class CourseController {
-}
